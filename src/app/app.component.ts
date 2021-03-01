486e9df20ddc94a7a1405019facc62300d5a141a
Diana Kohut <dianakogut.088@gmail.com>
2021-03-01 16:13:54 +0200

    basic table layout with sorting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { Movie } from './models/movie.model';
+import { MoviesService } from './services/movies.service';
+import { SetSortBy } from './state/movies.actions';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'trainingFilms';
+
+  movies: Array<Movie> = [];
+  sortBy: string = '';
+
+  constructor(private store: Store, private moviesService: MoviesService) { }
+
+  ngOnInit() {
+
+    this.store.subscribe(state => {
+      this.sortBy = state.moviesState.sortBy;
+    });
+
+    this.moviesService.getMovies()
+      .subscribe((data: Array<Movie>) => {
+        this.movies = data;
+      })
+  }
+
+  sortMovies(data) {
+    if (this.sortBy === data) {
+      this.movies = this.movies.reverse();
+    } else {
+      this.store.dispatch(new SetSortBy(data));
+      this.movies = this.movies.slice()
+        .sort((a, b) => a[this.sortBy] > b[this.sortBy] ? 1 : -1)
+    }
+  }
 }
---
+++ src/app/state/movies.state.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { State, Action, StateContext, Selector } from '@ngxs/store';
+import { MoviesService } from '../services/movies.service';
+import { SetSortBy } from './movies.actions';
+
+
+interface MoviesState {
+    sortBy: string;
+};
+
+@State<MoviesState>({
+    name: 'moviesState',
+    defaults: {
+        sortBy: ''
+    }
+})
+@Injectable()
+export class MoviesStore {
+
+    constructor(private readonly moviesService: MoviesService) { }
+
+    @Action(SetSortBy)
+    setSortBy({ patchState, setState, getState }: StateContext<MoviesState>, { sortBy }) {
+        const state = getState();
+        setState({ ...state, sortBy });
+    }
+
+}
